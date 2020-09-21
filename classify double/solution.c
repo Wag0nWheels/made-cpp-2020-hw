ddf2e0c71edcc82187a37090a42b2f64c64a467f
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2020-09-22 00:04:10 +0300

    added solution for the first task

--- classify double/solution.c
+++ classify double/solution.c
@@ -10,12 +10,36 @@
  * You should use them in the main sections.
  */
 
-uint64_t convertToUint64 (double number) {
-    return *((uint64_t *)(&number));
+uint64_t convertToUint64(double number) {
+    return *((uint64_t*)(&number));
 }
 
-bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+bool getBit(const uint64_t number, const uint8_t index) {
+    uint64_t mask = 1 << index;
+    uint64_t temp = mask & number;
+    temp >>= index;
+    return temp;
+}
+
+// проверка на нулевые значения
+bool it_is_0(const uint64_t number, uint8_t index_right, uint8_t index_left) {
+    uint8_t count = 0;
+    while (getBit(number, index_right) == 0 && index_right <= index_left) {
+        count++;
+        index_right++;
+    }
+    return count == 11;
+}
+
+// проверка на единичные значения
+bool it_is_1(const uint64_t number, uint8_t index_right, uint8_t index_left) {
+    uint8_t total = index_left - index_right + 1;
+    uint8_t count = 0;
+    while (getBit(number, index_right) == 1 && index_right <= index_left) {
+        count++;
+        index_right++;
+    }
+    return count == total;
 }
 
 
@@ -23,48 +47,82 @@ bool getBit (const uint64_t number, const uint8_t index) {
  * Checkers here:
  */
 
-bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+bool checkForPlusZero(uint64_t number) {
+    return number == 0x0000000000000000;
 }
 
-bool checkForMinusZero (uint64_t number) {
+bool checkForMinusZero(uint64_t number) {
     return number == 0x8000000000000000;
 }
 
-bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+bool checkForPlusInf(uint64_t number) {
+    return number == 0x7ff0000000000000;
 }
 
-bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+bool checkForMinusInf(uint64_t number) {
+    return number == 0xfff0000000000000;
 }
 
-bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusNormal(uint64_t number) {
+    if (getBit(number, 63) == 0) {
+        if (!it_is_1(number, 52, 62)) {
+            if (!it_is_0(number, 52, 62)) {
+                return true;
+            }
+        }
+    }
 }
 
-bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusNormal(uint64_t number) {
+    if (getBit(number, 63) == 1) {
+        if (!it_is_1(number, 52, 62)) {
+            if (!it_is_0(number, 52, 62)) {
+                return true;
+            }
+        }
+    }
 }
 
-bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusDenormal(uint64_t number) {
+    if (getBit(number, 63) == 0) {
+        if (it_is_0(number, 52, 62)) {
+            if (!it_is_0(number, 0, 51)) {
+                return true;
+            }
+        }
+    }
 }
 
-bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusDenormal(uint64_t number) {
+    if (getBit(number, 63) == 1) {
+        if (it_is_0(number, 52, 62)) {
+            if (!it_is_0(number, 0, 51)) {
+                return true;
+            }
+        }
+    }
 }
 
-bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+bool checkForSignalingNan(uint64_t number) {
+    if (it_is_1(number, 52, 62)) {
+        if (!it_is_0(number, 0, 51)) {
+            if (getBit(number, 51) == 0) {
+                return true;
+            }
+        }
+    }
 }
 
-bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+bool checkForQuietNan(uint64_t number) {
+    if (it_is_1(number, 52, 62)) {
+        if (getBit(number, 51) == 1) {
+            return true;
+        }
+    }
 }
 
 
-void classify (double number) {
+void classify(double number) {
     if (checkForPlusZero(convertToUint64(number))) {
         printf("Plus zero\n");
     }
@@ -109,3 +167,4 @@ void classify (double number) {
         printf("Error.\n");
     }
 }
+
