f39af8e94a1d731e8912db9f4f2ff8c3a5328e8f
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2020-09-29 22:46:28 +0300

    added solution for det

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,41 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+	ldr r1, [r0], #16
+	ldr r2, [r0], #16
+	mul r1, r1, r2
+	ldr r2, [r0], #-28
+	mul r1, r1, r2
+	ldr r2, [r0], #16
+	ldr r3, [r0], #4
+	mul r2, r2, r3
+	ldr r3, [r0], #4
+	mul r2, r2, r3
+	add r1, r1, r2
+	ldr r2, [r0], #-16
+	ldr r3, [r0], #-4
+	mul r2, r2, r3
+	ldr r3, [r0], #0
+	mul r2, r2, r3
+	add r1, r1, r2
+	ldr r2, [r0], #8
+	ldr r3, [r0], #8
+	mul r2, r2, r3
+	ldr r3, [r0], #4
+	mul r2, r2, r3
+	sub r1, r1, r2
+	ldr r2, [r0], #-8
+	ldr r3, [r0], #-20
+	mul r2, r2, r3
+	ldr r3, [r0], #4
+	mul r2, r2, r3
+	sub r1, r1, r2
+	ldr r2, [r0], #8
+	ldr r3, [r0], #20
+	mul r2, r2, r3
+	ldr r3, [r0]
+	mul r2, r2, r3
+	sub r1, r1, r2
+	mov r0, r1
+	bx lr
